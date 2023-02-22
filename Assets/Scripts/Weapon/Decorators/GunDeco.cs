a515103356a08c575f3456690abaa9376e981d2e
Mint <htminh.1001@gmail.com>
2023-02-22 15:21:25 +0700

    chore: minor refactor with IDE suggestions

--- Assets/Scripts/Weapon/Decorators/GunDeco.cs
+++ Assets/Scripts/Weapon/Decorators/GunDeco.cs
@@ -2,30 +2,31 @@ using UnityEngine;
 
 public class GunDeco : WeaponDecorator
 {
-  private float shootDirecitonMultiplier = 1f;
+    private float shootDirecitonMultiplier = 1f;
 
-  private GameObject master;
+    private readonly GameObject master;
 
-  public GunDeco(Weapon weapon, GameObject master) : base(weapon)
-  {
-    this.master = master;
-  }
-  public override void Fire()
-  {
-    base.Fire();
+    public GunDeco(Weapon weapon, GameObject master) : base(weapon)
+    {
+        this.master = master;
+    }
 
-    Shoot();
-  }
+    public override void Fire()
+    {
+        base.Fire();
 
-  private void Shoot()
-  {
-    // Shoot additional bullet
-    var bulletPrefab = Resources.Load("Prefabs/Bullet") as GameObject;
-    Bullet bullet = GameObject
-    .Instantiate(bulletPrefab, master.transform.position, master.transform.rotation)
-    .GetComponent<Bullet>();
+        Shoot();
+    }
 
-    bullet.Direction = Vector2.right * shootDirecitonMultiplier;
-    shootDirecitonMultiplier *= -1;
-  }
+    private void Shoot()
+    {
+        // Shoot additional bullet
+        var bulletPrefab = Resources.Load("Prefabs/Bullet") as GameObject;
+        Bullet bullet = Object
+        .Instantiate(bulletPrefab, master.transform.position, master.transform.rotation)
+        .GetComponent<Bullet>();
+
+        bullet.Direction = Vector2.right * shootDirecitonMultiplier;
+        shootDirecitonMultiplier *= -1;
+    }
 }
--- Assets/Scripts/Weapon/Decorators/WandDeco.cs
+++ Assets/Scripts/Weapon/Decorators/WandDeco.cs
@@ -2,38 +2,42 @@ using UnityEngine;
 
 public class WandDeco : WeaponDecorator
 {
-  private float shootDirecitonMultiplier = 1f;
-  private GameObject master;
+    private float shootDirecitonMultiplier = 1f;
+    private readonly GameObject master;
 
-  public WandDeco(Weapon weapon, GameObject master) : base(weapon)
-  {
-    this.master = master;
-  }
-  public override void Fire()
-  {
-    base.Fire();
-
-    CastSpell();
-  }
-
-  private void CastSpell()
-  {
-    // Shoot additional bullet
-    var magicPrefab = Resources.Load("Prefabs/Magic") as GameObject;
-
-    // Shoot 3 with random angle
-    for (int i = 0; i < 3; i++)
+    public WandDeco(Weapon weapon, GameObject master) : base(weapon)
     {
-      Vector3 offset = new Vector3(1, 1, 0);
-      Bullet magic = GameObject
-      .Instantiate(magicPrefab, master.transform.position + offset, master.transform.rotation)
-      .GetComponent<Bullet>();
-
-      magic.Direction = Quaternion.AngleAxis(Random.Range(-10, 10), Vector3.forward) *
-        Vector2.right *
-        shootDirecitonMultiplier;
+        this.master = master;
     }
 
-    shootDirecitonMultiplier *= -1;
-  }
+    public override void Fire()
+    {
+        base.Fire();
+
+        CastSpell();
+    }
+
+    private void CastSpell()
+    {
+        // Shoot additional bullet
+        var magicPrefab = Resources.Load("Prefabs/Magic") as GameObject;
+
+        // Shoot 3 with random angle
+        for (int i = 0; i < 3; i++)
+        {
+            Vector3 offset = new(1, 0, 0);
+            Bullet magic = Object
+            .Instantiate(
+                magicPrefab,
+                master.transform.position + offset * shootDirecitonMultiplier,
+                master.transform.rotation)
+            .GetComponent<Bullet>();
+
+            magic.Direction = Quaternion.AngleAxis(Random.Range(-10, 10), Vector3.forward) *
+              Vector2.right *
+              shootDirecitonMultiplier;
+        }
+
+        shootDirecitonMultiplier *= -1;
+    }
 }
